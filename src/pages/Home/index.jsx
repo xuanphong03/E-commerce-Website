28221f9817ad0706ef89df600d52ddc5a2eb5860
Michael Nguyen <ngxphong03@gmail.com>
2024-07-11 11:48:42 +0700

    Build introduce section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,11 @@
+import IntroduceSection from './components/IntroduceSection';
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <main className="bg-white">
+      <div className="mx-auto max-w-[1300px]">
+        <IntroduceSection />
+      </div>
+    </main>
+  );
 }
