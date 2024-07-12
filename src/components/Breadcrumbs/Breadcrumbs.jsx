8cf960f589d63cffeb2c5fe4a212ff8a0081baa5
Michael Nguyen <ngxphong03@gmail.com>
2024-07-12 16:20:42 +0700

    build error404_page

---
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+export default function Breadcrumbs({ pathList = [] }) {
+  return (
+    <p className="flex flex-row gap-2">
+      {pathList.map((path, index) => {
+        let isCurrentPath = index === pathList.length - 1;
+        return (
+          <Link
+            to={path.to}
+            className={`font-poppins text-sm ${isCurrentPath ? 'text-black' : 'text-[#808080]'}`}
+            key={path.id}
+          >
+            {path.name}
+            <span className="ml-2">{!isCurrentPath ? '/' : ''}</span>
+          </Link>
+        );
+      })}
+    </p>
+  );
+}
--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,3 +1,38 @@
+import { useParams } from 'react-router-dom';
+import Breadcrumbs from '~/components/Breadcrumbs/Breadcrumbs';
+
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  const paths = [
+    {
+      id: 1,
+      to: '/',
+      name: 'Home',
+    },
+    {
+      id: 2,
+      to: '/error',
+      name: '404 Error',
+    },
+  ];
+
+  return (
+    <main className="bg-white pb-40 pt-10">
+      <div className="mx-auto max-w-[1300px]">
+        <div className="mb-[140px]">
+          <Breadcrumbs pathList={paths} />
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="mb-10 font-inter text-[110px] font-medium leading-[115px] tracking-[3.3px]">
+            404 Not Found
+          </h1>
+          <p className="mb-20 font-poppins">
+            Không tìm thấy trang bạn đã truy cập. Bạn có thể vào trang chủ
+          </p>
+          <button className="flex items-center justify-center rounded bg-[#DB4444] px-12 py-4 text-[#FAFAFA]">
+            Quay về trang chủ
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
