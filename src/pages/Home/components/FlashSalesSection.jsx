a5c1c004ff225c1826d53df25c69ec0e8e071a7b
Michael Nguyen <ngxphong03@gmail.com>
2024-07-16 00:43:14 +0700

    create best selling product section

--- src/pages/Home/components/FlashSalesSection.jsx
+++ src/pages/Home/components/FlashSalesSection.jsx
@@ -4,14 +4,28 @@ import ProductImage from '~/assets/images/product01.png';
 import ProductItem from '~/components/ProductItem';
 
 export default function FlashSalesSection() {
-  const [timerDays, setTimerDays] = useState('0');
-  const [timerHours, setTimerHours] = useState('0');
-  const [timerMinutes, setTimerMinutes] = useState('0');
-  const [timerSeconds, setTimerSeconds] = useState('0');
+  const [timer, setTimer] = useState(() => {
+    const countdownDate = new Date('July 30, 2024 00:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  });
 
-  let interval = useRef();
+  const interval = useRef();
+
   const startTimer = () => {
-    const countdownDate = new Date('July 15, 2024 00:00:00').getTime();
+    const countdownDate = new Date('July 30, 2024 00:00:00').getTime();
 
     interval.current = setInterval(() => {
       const now = new Date().getTime();
@@ -25,14 +39,14 @@ export default function FlashSalesSection() {
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       if (distance < 0) {
-        // Stop timer
         clearInterval(interval.current);
       } else {
-        // Update timer
-        setTimerDays(days);
-        setTimerHours(hours);
-        setTimerMinutes(minutes);
-        setTimerSeconds(seconds);
+        setTimer({
+          days,
+          hours,
+          minutes,
+          seconds,
+        });
       }
     }, 1000);
   };
@@ -57,28 +71,28 @@ export default function FlashSalesSection() {
           <div>
             <h4 className="font-poppins text-xs font-medium">Days</h4>
             <p className="font-inter text-[32px] font-bold leading-[32px] tracking-[1.28px]">
-              {timerDays < 10 ? '0' + timerDays : timerDays}
+              {timer.days < 10 ? '0' + timer.days : timer.days}
             </p>
           </div>
           <div className="text-2xl font-bold text-[#E07575]">:</div>
           <div>
             <h4 className="font-poppins text-xs font-medium">Hours</h4>
             <p className="font-inter text-[32px] font-bold leading-[32px] tracking-[1.28px]">
-              {timerHours < 10 ? '0' + timerHours : timerHours}
+              {timer.hours < 10 ? '0' + timer.hours : timer.hours}
             </p>
           </div>
           <div className="text-2xl font-bold text-[#E07575]">:</div>
           <div>
             <h4 className="font-poppins text-xs font-medium">Minutes</h4>
             <p className="font-inter text-[32px] font-bold leading-[32px] tracking-[1.28px]">
-              {timerMinutes < 10 ? '0' + timerMinutes : timerMinutes}
+              {timer.minutes < 10 ? '0' + timer.minutes : timer.minutes}
             </p>
           </div>
           <div className="text-2xl font-bold text-[#E07575]">:</div>
           <div>
             <h4 className="font-poppins text-xs font-medium">Seconds</h4>
             <p className="font-inter text-[32px] font-bold leading-[32px] tracking-[1.28px]">
-              {timerSeconds < 10 ? '0' + timerSeconds : timerSeconds}
+              {timer.seconds < 10 ? '0' + timer.seconds : timer.seconds}
             </p>
           </div>
         </div>
